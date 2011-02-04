051db54a0a46e8e2e02b4e86b69831a293d3785a
Julien <julien@nepsilon.net>
2011-02-05 02:21:31 +0800

    Removed testing code.

--- trie.py
+++ trie.py
@@ -59,23 +59,3 @@ class Trie():
             else:
                 return False
         return True
-
-def Trie_test():
-
-    words = ['china', 'chinese', 'karate', 'chineses']
-
-    S = Trie()
-
-    for w in words:
-        S.insert(w)
-
-    S.dump()
-
-    assert S.has_word('karate') is True
-    assert S.has_word('karat') is False
-    assert S.has_word('karatee') is False
-    assert S.has_word('judo') is False
-
-    assert S.has_prefix('kar') is True
-    assert S.has_prefix('karr') is False
-    assert S.has_prefix('kir') is False
